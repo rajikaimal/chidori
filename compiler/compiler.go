be66fa9f1c752c393495d52106012e1e3a9e8114
rajikaimal <inbox.rajika@gmail.com>
2020-06-12 01:00:14 +0530

    Add compiler pkg

---
+++ compiler/compiler.go
@@ -0,0 +1,43 @@
+package compiler
+
+import (
+	"go/token"
+	"log"
+	"os"
+
+	"github.com/goruby/goruby/evaluator"
+	"github.com/goruby/goruby/object"
+	"github.com/goruby/goruby/parser"
+)
+
+// Interpreter defines the methods of an interpreter
+type Compiler interface {
+	Compile(filename string, input interface{}) (object.RubyObject, error)
+}
+
+// New returns an Compiler ready to use and with the environment set to
+// object.NewMainEnvironment()
+func New() Compiler {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Printf("Cannot get working directory: %s\n", err)
+	}
+	env := object.NewMainEnvironment()
+	loadPath, _ := env.Get("$:")
+	loadPathArr := loadPath.(*object.Array)
+	loadPathArr.Elements = append(loadPathArr.Elements, &object.String{Value: cwd})
+	env.SetGlobal("$:", loadPathArr)
+	return &compiler{environment: env}
+}
+
+type compiler struct {
+	environment object.Environment
+}
+
+func (i *compiler) Compile(filename string, input interface{}) (object.RubyObject, error) {
+	node, err := parser.ParseFile(token.NewFileSet(), filename, input, 0)
+	if err != nil {
+		return nil, object.NewSyntaxError(err)
+	}
+	return evaluator.Eval(node, i.environment)
+}
